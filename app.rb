df0624097a86d9c6577a5feb8cdb987c0fc555e3
valogopedina <valentina.monetta@tiscali.it>
2016-02-02 16:31:44 +0000

    Added form

--- app.rb
+++ app.rb
@@ -12,7 +12,13 @@ get '/new_stuff' do
   'Hello world'
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
